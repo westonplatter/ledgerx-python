fcba52a5c3eac61a3f459d04943649cf27b107ba
westonplatter <westonplatter@gmail.com>
2021-02-10 10:45:24 -0700

    fix tests. add tests

--- ledgerx/generic_resource.py
+++ ledgerx/generic_resource.py
@@ -1,25 +1,25 @@
 from time import sleep
-from typing import List, Dict
+from typing import List, Dict, Callable
 import pandas as pd
 
 from ledgerx import DELAY_SECONDS
-from ledgerx.client import Client
+from ledgerx.http_client import HttpClient
 from ledgerx.util import gen_headers, gen_url, has_next_url
 
 
 class GenericResource:
     @classmethod
     def next(cls, next_url: str):
-        res = Client.get(next_url)
+        res = HttpClient.get(next_url)
         return res.json()
 
     @classmethod
     def list(cls, url: str, params: Dict):
-        res = Client.get(url, params)
+        res = HttpClient.get(url, params)
         return res.json()
 
     @classmethod
-    def list_all(cls, url: str, params: Dict = {}, max_fetches=0) -> List[Dict]:
+    def list_all(cls, url: str, params: Dict = {}, max_fetches: int=0) -> List[Dict]:
         elements = []
 
         json_data = cls.list(url, params)
@@ -29,8 +29,24 @@ class GenericResource:
             sleep(DELAY_SECONDS)
             json_data = cls.next(json_data["meta"]["next"])
             elements.extend(json_data["data"])
-            df = pd.DataFrame.from_dict(elements)
-            df.to_csv("all_trades.csv")
-            print(f"data = {len(elements)}")
+
+            # TODO(weston) for example, move this functionality into the list_all_incremental_return
+            # df = pd.DataFrame.from_dict(elements)
+            # df.to_csv("all_trades.csv")
+            # print(f"data = {len(elements)}")
 
         return elements
+
+    @classmethod
+    def list_all_incremental_return(cls, url: str, params: Dict={}, max_fetches: int=0, callback: Callable=None) -> None:
+        json_data = cls.list(url, params)
+        callback(json_data["data"])
+
+        while has_next_url(json_data):
+            sleep(DELAY_SECONDS)
+            json_data = cls.next(json_data["meta"]["next"])
+            callback(json_data["data"])
+
+
+
+
---
+++ tests/test_contracts.py
@@ -0,0 +1,8 @@
+from ledgerx import contracts
+
+def test_methods():
+    class_methods = dir(contracts.Contracts)
+    assert 'next' in class_methods
+    assert 'list' in class_methods
+    assert 'list_all' in class_methods
+    assert 'list_all_expiration_dates' in class_methods
