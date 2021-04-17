ba615ce8c6cc0777db214c2f0b4237c485f44918
westonplatter <westonplatter@gmail.com>
2021-04-17 11:44:51 -0600

    add BTC and ETH vols

---
+++ examples/bitvol_btc.py
@@ -0,0 +1,16 @@
+import ledgerx
+from examples.example_util import get_env_api_key
+
+ledgerx.api_key = get_env_api_key()
+
+
+resolutions = ["1H", "1D", "1W", "1M", "3M", "1Y"]
+
+for res in resolutions:
+    params = {"resolution": res}
+    data = ledgerx.Bitvol.list_btc(params)
+    print("-------------------")
+    print(f"params={params} returns {len(data['data'])} data points")
+    print(f"First vol object={data['data'][-1]}")
+    print(f"Second vol object={data['data'][-2]}")
+    print(f"Second vol object={data['data'][-3]}")
